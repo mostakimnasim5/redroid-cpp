a869b404cf6fbd2db22b9c4adcd41426fae73da3
mostakimnasim5 <mostakimnasim5@gmail.com>
2026-08-19 05:59:06 +0600

    chore: remove legacy gui/ folder (gui/settings_dialog.h)

--- gui/settings_dialog.h
+++
@@ -1,9 +0,0 @@
-#ifndef SETTINGS_DIALOG_H
-#define SETTINGS_DIALOG_H
-#include <QDialog>
-class SettingsDialog : public QDialog {
-    Q_OBJECT
-public:
-    explicit SettingsDialog(QWidget* parent = nullptr);
-};
-#endif
